5552c3663470d93eb671fd50c49aa12f95a9fde6
Siprachanh <sipra.chant@gmail.com>
2022-02-10 15:15:09 -0600

    Dynamically created matchday using dropdown box affordance

--- src/components/invites/InviteManager.js
+++ src/components/invites/InviteManager.js
@@ -1,3 +1,15 @@
 //contains fetch calls to the API
 
 const remoteURL = "http://localhost:8088"
+export const getMatchDays = () => {
+    return fetch(`${remoteURL}/matchDay?`)
+    .then(res => res.json())
+}
+
+
+// const remoteURL = "http://localhost:8088"
+// export const getMatchDays = () => {
+//     return fetch(`${remoteURL}/matchDay?`)
+//     .then(res => res.json())
+// }
+
